d8973299a88ca2963cc67af25cb1a7899b1b9f88
IgorFlp <igor.felipe.13@hotmail.com>
2026-01-19 15:09:32 -0300

    Added files pages (with download option)

---
+++ public/files.js
@@ -0,0 +1,72 @@
+window.onerror = function (msg, url, line) {
+  alert("JS ERROR:\n" + msg + "\nline: " + line);
+};
+
+function httpGet(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+function CreateFileListItem(file) {
+  var span = document.createElement("span");
+  var actionSpan = document.createElement("span");
+  var downloadBtn = document.createElement("a");
+  var editBtn = document.createElement("label");
+  var nameLabel = document.createElement("label");
+
+  span.className = "file-list-item";
+  actionSpan.className = "file-actions";
+  downloadBtn.className = "btn download-icon";
+  downloadBtn.href = "/download/" + file;
+  editBtn.className = "btn edit-icon";
+  nameLabel.className = "file-name-label";
+
+  nameLabel.textContent = file;
+  actionSpan.appendChild(downloadBtn);
+  actionSpan.appendChild(editBtn);
+  span.appendChild(nameLabel);
+  span.appendChild(actionSpan);
+  return span;
+}
+window.onload = function () {
+  httpGet("/api/files", function (files) {
+    var filePage = document.querySelector(".file-list");
+    files = files.files;
+    files.forEach(function (file) {
+      var li = document.createElement("li");
+      var pasta = file.split("\\")[0];
+      var nome = file.split("\\")[1];
+      if (nome !== undefined) {
+        var htmlPasta = document.getElementById(pasta);
+        if (htmlPasta) {
+          var span = CreateFileListItem(nome);
+          var fileList = htmlPasta.querySelector(".file-list");
+          fileList.appendChild(span);
+        } else {
+          var novaPasta = document.createElement("section");
+          var title = document.createElement("h2");
+          var fileList = document.createElement("div");
+          fileList.className = "file-list";
+
+          title.textContent = pasta;
+          novaPasta.id = pasta;
+
+          var span = CreateFileListItem(nome);
+
+          novaPasta.appendChild(title);
+          fileList.appendChild(span);
+          novaPasta.appendChild(fileList);
+          filePage.appendChild(novaPasta);
+        }
+      } else {
+        var span = CreateFileListItem(file);
+        filePage.appendChild(span);
+      }
+    });
+  });
+};
